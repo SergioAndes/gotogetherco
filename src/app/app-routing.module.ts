4bf5fcda795d629005d4bd64c7dec3929e1b93bd
SergioAndes <54184733+SergioAndes@users.noreply.github.com>
2021-09-13 18:21:17 -0500

    sliders

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {HomeComponent} from './home/home.component';
 import {LoginComponent} from "./login/login.component";
+import {SwiperHomeComponent} from "./swiper-home/swiper-home.component";
 
 
 
@@ -16,6 +17,7 @@ const routes: Routes = [
     pathMatch: 'full'
   },
   {path: 'login', component: LoginComponent},
+  {path: 'swiper', component: SwiperHomeComponent},
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,17 +7,27 @@ import { SplashScreenComponent } from './splash-screen/splash-screen.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { HashLocationStrategy, LocationStrategy } from '@angular/common';
+import { SwiperHomeComponent } from './swiper-home/swiper-home.component';
+import { SwiperModule } from 'swiper/angular';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from "@angular/material/button";
 
 @NgModule({
   declarations: [
     AppComponent,
     SplashScreenComponent,
     HomeComponent,
-    LoginComponent
+    LoginComponent,
+    SwiperHomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SwiperModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatButtonModule
   ],
   providers: [{provide: LocationStrategy, useClass: HashLocationStrategy}],
   bootstrap: [AppComponent]
