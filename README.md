e04088434a9dd3be9a00b4dd8dbcf5161fa66acc
SergioAndes <54184733+SergioAndes@users.noreply.github.com>
2021-10-28 09:41:26 -0500

    Revert "Feature/chat"

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Gotogether
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 10.0.0-next.0. and sockets
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 10.0.0-next.0.
 
 ## Development server
 
--- src/app/chatlist/chatlist.component.ts
+++ src/app/chatlist/chatlist.component.ts
@@ -1,111 +1,15 @@
-import { Component, OnInit, Injectable } from '@angular/core';
-import { Socket } from 'ngx-socket-io';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-chatlist',
   templateUrl: './chatlist.component.html',
   styleUrls: ['./chatlist.component.css']
 })
-@Injectable({
-  providedIn: 'root'
-})
 export class ChatlistComponent implements OnInit {
 
-  httpOptions = {
-    headers: new HttpHeaders({ 
-      'Access-Control-Allow-Origin':'*'
-    })
-  };
-
-  title = 'angular-chat';
-  channel: boolean = true;
-
-  messages = [];
-  newMessage = '';
-  channelList = []// = ["pedro", "pablo", "camila"]; //mockData
-
-  currentUser: string = JSON.parse(localStorage.getItem("user")).username;
-  userToChat: string;
-  room = "room1";
-
-  getMessagesPath = "http://localhost:3000/messages?me="+this.currentUser+"&"+"someone=";
-
-  //const user = { id, username, room, connected: true }; Example
-  //const message = {username, text, createdAt}
-
-  constructor(private socket: Socket, private http: HttpClient) { 
-
-    this.joinChat();
-    this.getUsers();
-    this.getMessagesFromServer();
-    this.receieveMessages();
-  }
+  constructor() { }
 
   ngOnInit(): void {
-
-  }
-
-  joinChat() {
-    this.socket.emit('join', { username: this.currentUser, room: this.room }, (error) => {
-      if (error) {
-          alert(error)
-          location.href = '/'
-      }
-    })
-  }
-
-  getUsers() {
-    this.socket.on('roomData', ({ room, users }) => {
-      this.channelList = users;
-      this.channelList = this.channelList.filter(u => u.username !== this.currentUser)
-      this.channelList.sort((u1, u2) => u1.username < u2.username ? -1 : 1);
-    })
-  }
-
-  receieveMessages() {
-    this.socket.on('privateMessage', (msg) => {
-      if(msg.username == this.userToChat) {
-        this.messages.push(msg);
-      }
-    })
-  }
-
-  sendMessage() {
-    if (this.newMessage.trim() === '') {
-      return;
-    }
-
-    try {
-      let msg = {text: this.newMessage, to: this.userToChat, from: this.currentUser}
-      this.socket.emit('privateMessage', msg, (error) => {
-        if(error)
-          console.log(error);
-      });
-      this.newMessage = '';
-      this.messages.push(msg);
-    } catch (err) {
-      console.log(err);
-    }
-  }
-
-  onSelect(channel) {
-    //console.log(channel)
-    //Load chats
-
-    this.messages = [] //TODO: Load messages from that user
-    this.userToChat = channel.username;
-
-    this.getMessagesFromServer();
-
-  }
-
-  getMessagesFromServer() {
-    let route = this.getMessagesPath + this.userToChat;
-    this.http.get(route, this.httpOptions).subscribe(msgs => {
-      this.messages = Object.keys(msgs).map(key => (msgs[key]));
-      console.log(this.messages);
-    });
   }
 
 }
